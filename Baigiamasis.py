eab484635c8c3a1953fa34a4989356fa5be68f1d
Laurynas Lašas <103446704+LaurynasLasas@users.noreply.github.com>
2022-04-11 21:11:50 +0300

    Add files via upload

---
+++ Baigiamasis.py
@@ -0,0 +1,35 @@
+#Libraries
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+   
+#Webscraping
+
+r = requests.get('https://rrr.lt/naudotos-autodalys/honda/cr-v-1995-2001#parts-1')
+bs = BeautifulSoup(r.text, 'html.parser')
+results = bs.find_all('div', attrs={'class': 'products_slider__hold'})
+records = []
+
+#Database
+
+for result in results:
+    url = result.find('a')['href']
+    records.append(url)
+df = pd.DataFrame(records, columns=['Nuorodos'])
+df.to_csv('Naujausios_detales.csv', index = False, sep=',', encoding = 'utf-8')
+df.reset_index(drop=True)
+        
+#Duplicate removal        
+
+data = pd.read_csv('Naujausios_detales.csv')
+data.drop_duplicates(subset ="Nuorodos", keep = "first", inplace = True)
+data.to_csv('Naujausios_detales.csv', index = False)
+
+#Automation
+
+#Automation is done trough windows scheduler
+
+
+
+
